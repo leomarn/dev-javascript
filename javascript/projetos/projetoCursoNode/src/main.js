c42ca89c2ba70460ac1809a37746432c3f2f5e0f
Leomar Nogueira <leomar_n@outlook.com>
2022-09-06 14:43:57 -0300

    atualizacoes v2

--- javascript/projetos/projetoCursoNode/src/main.js
+++ javascript/projetos/projetoCursoNode/src/main.js
@@ -1,11 +1,43 @@
 const port = 3003
 
+
 const express = require('express')
-
 const app = express()
+const bodyParser = require('body-parser')
+const dataBase = require('./database')
+const { reset } = require('nodemon')
 
-app.get('/products', (req, res, next) => {
-    res.send({name: 'Leomar', phoneNumber: 91980873042})
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/products', (req, res) => {
+    res.send(dataBase.getProducts())
 })
+
+app.get('/products/:id', (req, res) => {
+    res.send(dataBase.getProduct(req.params.id))
+})
+
+app.post('/products', (req, res) => {
+    const product = dataBase.saveProducts({
+        name: req.body.name,
+        price: req.body.price
+    })
+    res.send(product)
+})
+
+app.put('/products/:id', (req, res) => {
+    const product = dataBase.saveProducts({
+        id: req.params.id,
+        name: req.body.name,
+        price: req.body.price
+    })
+    res.send(product) //JSOM
+})
+
+app.delete('/products/:id', (req, res) => {
+    const product = dataBase.deleteProduct(req.params.id)
+    res.send(product)
+})
+
 
 app.listen(port, () => console.log(`Open server in port: ${port}`))
